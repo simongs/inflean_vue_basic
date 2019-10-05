cc4a248cb366370c959342e6f68604a6c590bf9c
chopokmado <chopokmado@gmail.com>
2019-10-06 08:52:21 +0900

    .

---
+++ src/a.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    // 인스턴스 옵션
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+      <h1>{{dataString}}</h1>
+      <button v-on:click="sendEvent">Click</button>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['dataString'],
+  methods: {
+    sendEvent: function() {
+      this.$emit('pass');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
