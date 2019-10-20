5891d4efdeeda2aed82f8d251d7b874d1544454a
chopokmado <chopokmado@gmail.com>
2019-10-20 21:04:11 +0900

    메소드를 파일로 분리하는 방법

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import * as getters from './comp/getters'
+import * as mutations from './comp/mutations'
+
 // Vue의 플러그인이라는 기능
 // 글로벌 펑셔널리티를 추가하고 싶을 때 사용
 // 특정 컴포넌트(ex. TodoComponent) 에서 this.$store 라고 접근할 수 있게 해줌
@@ -27,35 +30,6 @@ export const store = new Vuex.Store({
         headerText : "TODO It!", 
         todoItems : storage.fetch()
     },
-    getters : {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations : {
-        addOneItem(state, newTodoItem) {
-            const obj = {completed: false, item: newTodoItem};
-            localStorage.setItem(newTodoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);  
-        },
-
-        deleteTodoItem(state, dataMap) {
-            localStorage.removeItem(dataMap.todoItem.item);
-            state.todoItems.splice(dataMap.index, 1); // index부터 1개를 지우겠다.
-        }, 
-
-        toggleOneItem(state, dataMap) {
-            // 로컬스토리지 쪽은 업데이트 기능이 없다 (삭제 + 삽입)
-            // (안티패턴이다) - 컨테이너로 내린 데이터가 다시 올라와서 컨테이너에서 수정되는 것은 좋지 않다.
-            // todoItem.completed = !todoItem.completed;
-            state.todoItems[dataMap.index].completed = !dataMap.todoItem.completed;
-
-            localStorage.removeItem(dataMap.todoItem.item);
-            localStorage.setItem(dataMap.todoItem.item, JSON.stringify(dataMap.todoItem));
-        },
-        deleteAllTodoItem(state) {
-            localStorage.clear();
-            state.todoItems = [];
-        }
-    }
+    getters : getters,
+    mutations : mutations
 });
